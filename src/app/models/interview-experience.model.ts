3ea9ff4f6f18a475ca9129d5fb9e09cad23f5071
Dinesh <dineshbachchani1234@gmail.com>
2025-04-17 00:47:12 -0400

    student ui complete

--- src/app/models/interview-experience.model.ts
+++ src/app/models/interview-experience.model.ts
@@ -2,9 +2,19 @@ export interface InterviewExperience {
   experienceID?: number;
   interview: {
     interviewId: number;
+    application?: {
+      job?: {
+        title?: string;
+        company?: {
+          companyName?: string;
+        }
+      }
+    };
   };
   student: {
     studentId: number;
+    firstName?: string;
+    lastName?: string;
   };
   comment: string;
   rating: number;
